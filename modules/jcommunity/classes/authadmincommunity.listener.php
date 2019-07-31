e4545dc93904a7db83adba54973691db4b8977cf
Laurent Jouanneau <dev@ljouanneau.com>
2019-07-31 14:35:20 +0200

    Dedicated controller for the admin to resend validation email

--- modules/jcommunity/classes/authadmincommunity.listener.php
+++ modules/jcommunity/classes/authadmincommunity.listener.php
@@ -32,10 +32,20 @@ class authadmincommunityListener extends jEventListener{
             $tpl = $event->tpl;
             $tpl->assign('canChangePass', false);
             $links = $tpl->get('otherLinks');
-            $links[] = array(
-                'url' => jUrl::get('jcommunity~password_reset_admin:index', array('login'=>$tpl->get('id'))),
-                'label' => jLocale::get('jcommunity~account.admin.link.account.reset.password'),
-            );
+            $status = $form->getData('status');
+            $keyactivate = $form->getData('keyactivate');
+            if ($status == \Jelix\JCommunity\Account::STATUS_NEW) {
+                $links[] = array(
+                    'url' => jUrl::get('jcommunity~registration_admin_resend:index', array('login'=>$tpl->get('id'))),
+                    'label' => jLocale::get('jcommunity~account.admin.link.account.resent.validation.email'),
+                );
+            }
+            else {
+                $links[] = array(
+                    'url' => jUrl::get('jcommunity~password_reset_admin:index', array('login'=>$tpl->get('id'))),
+                    'label' => jLocale::get('jcommunity~account.admin.link.account.reset.password'),
+                );
+            }
             $tpl->assign('otherLinks', $links);
         }
     }
